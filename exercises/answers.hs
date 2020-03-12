f85988e5cb05ee9ad59aa8a1bbd8c073223a0ac9
Ewerton Lima <ehzcl@cin.ufpe.br>
2020-03-12 16:41:15 -0300

    answers to the exercises of lesson #2

--- exercises/answers.hs
+++ exercises/answers.hs
@@ -44,3 +44,84 @@ vendasIguais :: Int -> Int -> Int
 vendasIguais s n
  | n == 0 = igual s (vendas 0)
  | otherwise = (igual s (vendas n)) + vendasIguais s (n-1)
+
+-- lesson #2 exercises
+
+totalVendas :: Int -> Int
+totalVendas x
+ | x == 0 = vendas 0
+ | otherwise = vendas x + totalVendas (x-1)
+
+addEspacos :: Int -> String
+addEspacos e
+ | e == 0 = ""
+ | otherwise = " " ++ addEspacos (e-1)
+
+paraDireita :: Int -> String -> String
+paraDireita i s = addEspacos i ++ s
+
+imprimeTabela :: Int -> String
+imprimeTabela n = cabecalho
+                  ++ imprimeSemanas n
+                  ++ imprimeTotal n
+                  ++ imprimeMedia n
+
+cabecalho :: String
+cabecalho = paraDireita 5 ("Semana" ++ addEspacos 4 ++ "Venda" ++ "\n")
+
+imprimeSemanas :: Int -> String
+imprimeSemanas x
+ | x == 0 = "\t" ++ "0" ++ "\t" ++ (show (vendas 0)) ++ "\n"
+ | otherwise = imprimeSemanas (x-1) ++ "\t" ++ (show x) ++ "\t" ++ (show (vendas x)) ++ "\n"
+
+imprimeTotal :: Int -> String
+imprimeTotal x = paraDireita 6 ("Total" ++ "\t" ++ show (totalVendas x) ++ "\n")
+
+imprimeMedia :: Int -> String
+imprimeMedia x = paraDireita 6 ("Média" ++ "\t" ++ show ( fromIntegral (totalVendas x )/ fromIntegral (x+1)) ++ "\n")
+
+impressao :: Int -> IO()
+impressao n = putStr (imprimeTabela n)
+
+menorMaior :: Int -> Int -> Int -> (Int, Int)
+menorMaior x y z = (a,b)
+  where
+   a = min x (min y z)
+   b = max x (max y z)
+
+ordena2 :: (Int, Int) -> (Int, Int)
+ordena2 (x, y)
+ | x > y = (y, x)
+ | otherwise = (x,y)
+
+diff :: Int -> Int -> Int -> Int -> Int -> Int
+diff x y z a b
+ | x /= a && x /= b = x
+ | y /= a && x /= b = y
+ | z /= a && z /= b = z
+
+ordenaTripla :: (Int, Int, Int) -> (Int, Int, Int)
+ordenaTripla (x, y, z) = (a, b, c)
+  where
+    a = min x (min y z)
+    c = max x (max y z)
+    b = diff x y z a c
+
+type Ponto = (Float, Float)
+type Reta  = (Ponto, Ponto)
+
+fstPonto :: Ponto -> Float
+fstPonto x = fst x
+
+sndPonto :: Ponto -> Float
+sndPonto (_,y) = y
+
+retaVertical :: Reta -> Bool
+retaVertical x 
+  | fstPonto(fst x) == fstPonto(snd x) = True
+  | otherwise = False
+ 
+pontoY :: Float -> Reta -> Float
+pontoY fl rt = (((sndPonto (snd rt) - sndPonto (fst rt))
+                 *(fl - fstPonto (fst rt)))
+                 /(fstPonto (snd rt) - fstPonto (fst rt))) + sndPonto (fst rt)
